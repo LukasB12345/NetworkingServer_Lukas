09efdddead03584d52564031f9fe877e0b7111c4
lukas.bommelin <lukas.bommelin@elev.ga.ntig.se>
2024-05-06 10:41:54 +0200

    commita ofta, skapat en del av koden och valt ett portnumber

---
+++ src/NetworkingServer.java
@@ -0,0 +1,17 @@
+/* SERVER - may be enhanched to work for multiple clients */
+import java.net.*;
+import java.io.*;
+
+public class NetworkingServer {
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+        Socket client;
+
+        //Default port number we are going to use
+        int portnumber = 2005;
+        if (args.length >= 1){
+
+        }
+        }
+    }
